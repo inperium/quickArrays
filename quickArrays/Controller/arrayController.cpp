40c5c52388a8f951b5d47966da2edd500fa9e35f
inperium <ibow9416@740s-pl-111.local>
2017-02-06 12:42:30 -0700

    Created Arrays

---
+++ quickArrays/Controller/arrayController.cpp
@@ -0,0 +1,9 @@
+//
+//  arrayController.cpp
+//  quickArrays
+//
+//  Created by Bowen, Isaac on 2/6/17.
+//  Copyright © 2017 Bowen, Isaac. All rights reserved.
+//
+
+#include "arrayController.hpp"
---
+++ quickArrays/Controller/arrayController.hpp
@@ -0,0 +1,14 @@
+//
+//  arrayController.hpp
+//  quickArrays
+//
+//  Created by Bowen, Isaac on 2/6/17.
+//  Copyright © 2017 Bowen, Isaac. All rights reserved.
+//
+
+#ifndef arrayController_hpp
+#define arrayController_hpp
+
+#include <stdio.h>
+
+#endif /* arrayController_hpp */
